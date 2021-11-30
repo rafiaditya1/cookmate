a829b749f1f8a812d969749ab6bcc98560578d4a
Rafi Aditya Seno Aji <rafiaditya87@gmail.com>
2021-11-30 20:16:15 +0700

    Membuat FirstPage
    Membuat FirstPage

---
+++ lib/first_page.dart
@@ -0,0 +1,121 @@
+import 'package:cookmate/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: new AssetImage('assets/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 230,
+            left: 33,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cooking a',
+                  style: whiteTextStyle.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  'Delicious Food',
+                  style: orangeTextStyle.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  'Easily!',
+                  style: whiteTextStyle.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 160,
+            left: 33,
+            child: Text(
+              'CookingMate App bisa memberikanmu \nrekomendasi resep mudah, berdasarkan kategori \nresep yang ingin kamu buat.',
+              style: whiteTextStyle.copyWith(
+                fontWeight: medium,
+                fontSize: 12,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 94,
+            left: 34,
+            right: 29,
+            child: SizedBox(
+              width: 297,
+              height: 39,
+              child: ElevatedButton(
+                child: Text(
+                  'Daftar',
+                  style: whiteTextStyle.copyWith(
+                    fontWeight: bold,
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(orangeColor),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(33.0),
+                      side: BorderSide(color: orangeColor),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 34,
+            right: 29,
+            child: SizedBox(
+              width: 297,
+              height: 39,
+              child: ElevatedButton(
+                child: Text(
+                  'Masuk',
+                  style: orangeTextStyle.copyWith(
+                    fontWeight: bold,
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(whiteColor),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(33.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color orangeColor = Color(0xffE57758);
+Color lighOrangeColor = Color(0xffFDC18A);
+Color whiteColor = Color(0xffFFFFFF);
+Color blackColor = Color(0xff000000);
+Color greyColor = Color(0xffC4C4C4);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle orangeTextStyle = GoogleFonts.poppins(
+  color: orangeColor,
+);
+
+FontWeight bold = FontWeight.w900;
+FontWeight semiBold = FontWeight.w600;
+FontWeight medium = FontWeight.w500;
+FontWeight light = FontWeight.w300;
